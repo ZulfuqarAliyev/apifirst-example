c5805259ebb2a4c0e35697e57a65a47239be419c
Zulfugar Aliyev <zulfugar.aliyev@pashabank.az>
2025-01-23 12:24:42 +0400

    Initial commit

---
+++ src/main/java/az/zulfugar/apifirstexample/ApifirstExampleApplication.java
@@ -0,0 +1,13 @@
+package az.zulfugar.apifirstexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApifirstExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApifirstExampleApplication.class, args);
+    }
+
+}
